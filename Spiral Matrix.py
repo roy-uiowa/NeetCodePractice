fe2bcd20e0d81736843d82bdd3e9543224aa7d74
Tarun Kanti Roy <56485384+roy-uiowa@users.noreply.github.com>
2025-10-30 23:50:30 -0500

    Implement spiral order traversal of matrix

---
+++ Spiral Matrix.py
@@ -0,0 +1,24 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        l, r = 0, len(matrix[0])
+        t, b = 0, len(matrix)
+        res = []
+        while l < r and t < b:
+            #left to right
+            for i in range(l, r):
+                res.append(matrix[t][i])
+            t += 1
+            #top to bottom
+            for i in range(t, b):
+                res.append(matrix[i][r-1])
+            r -= 1
+            if not (l < r and t < b): break
+            #right to left
+            for i in range(r-1, l-1, -1):
+                res.append(matrix[b-1][i])
+            b -= 1
+            #bottom to top
+            for i in range(b-1, t-1, -1):
+                res.append(matrix[i][l])
+            l += 1
+        return res
